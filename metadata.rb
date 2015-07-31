06d3811c5c5f99ca96eb11895a53e275347cb736
Zak Crawford <zcrawford@angelrush.com>
2015-07-31 11:19:26 -0700

    bump elasticsearch version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adrien.brault@gmail.com'
 license          'MIT'
 description      'ElasticSearch OpsWorks'
 long_description 'ElasticSearch OpsWorks'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'elasticsearch'
 depends 'java'
